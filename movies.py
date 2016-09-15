4f018c87d59550e88d73018d6e332491622a1196
rahul ramaswami <rramaswami@localhost.localdomain>
2016-09-14 21:35:55 -0500

    review fixes

--- movies.py
+++ movies.py
@@ -6,7 +6,9 @@ import tmdbsimple as tmdb
 
 tmdb.API_KEY = '65992653354d8113805cb2a26a54c933'
 
+
 class Movie:
+
     def __init__(self, title, poster_image_num, trailer_youtube_id):
         self.title = title
         self.poster_image_url = Movie.poster_base_url + poster_image_num
@@ -14,7 +16,7 @@ class Movie:
 
     @classmethod
     def make_movie(cls, movieTitle):
-        # Return movie object if we find a match in theMovieDb Database, otherwise 
+        # Return movie object if we find a match in theMovieDb Database, otherwise
         # return None.
 
         search = tmdb.Search()
@@ -33,22 +35,21 @@ class Movie:
             if(videos['type'] == "Trailer" and videos['site'] == "YouTube"):
                 youtube_key = videos['key']
                 break
-        
-        #print json.dumps(s.__dict__) 
-        
+
+        # print json.dumps(s.__dict__)
+
         if(response):
             return Movie(s.title, s.poster_path, youtube_key)
         else:
             return None
-        
+
+
 def initOnce():
     # get the base_url used for full poster url.
     s = tmdb.Configuration().info()
     Movie.poster_base_url = s['images']['base_url'] + "original"
 
-initOnce();
-movies = { Movie.make_movie("The Force Awakens"),
-        Movie.make_movie("Bourne Iden"),
-        Movie.make_movie("Slumdog"),
-        Movie.make_movie("Warcraft") }
-fresh_tomatoes.open_movies_page(movies)
+initOnce()
+movies = {"The Force Awakens", "Bourne Iden",
+          "Slumdog", "Warcraft"}
+fresh_tomatoes.open_movies_page([Movie.make_movie(movie) for movie in movies])
